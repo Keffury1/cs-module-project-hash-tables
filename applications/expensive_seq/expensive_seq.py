2a2b8a6810d2de4a7d798dadd4b7ce6d321bd2a0
Robert Keffury <bobby.keffury@me.com>
2020-09-08 09:23:59 -0700

    Completed Expensive Sequence

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,9 +1,15 @@
-# Your code here
 
+my_dict = {}
 
 def expensive_seq(x, y, z):
-    # Your code here
-
+    if x <= 0 :
+        return y + z
+    elif (x,y,z) in my_dict:
+        value = my_dict.get((x,y,z))
+    else:
+        value = expensive_seq(x-1, y+1, z) + expensive_seq(x-2, y+2, z*2) + expensive_seq(x-3, y+3, z*3)
+        my_dict.update({ (x,y,z): value})
+    return value
 
 
 if __name__ == "__main__":
